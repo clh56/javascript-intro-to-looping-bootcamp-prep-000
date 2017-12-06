de0f1d96495e6f2a8b27ad6dc48fcce3e1e773ac
ciara hudson <cchudson33@gmail.com>
2017-12-06 05:25:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 for (let i=0; 1<25; i++) {
   if (i==="1") {
-  console.log("I am 1 strange loop")  
+  console.log("I am 1 strange loop")
   }
   else if (i=== 1<25){
     console.log("I am ${i} strange loops")
